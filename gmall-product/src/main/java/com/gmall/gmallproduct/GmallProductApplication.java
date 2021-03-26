e2d654851cb27820c6fa04670f2a5f3c836084f9
luzhibin <1255863672@qq.com>
2021-03-26 16:13:53 +0800

    项目结构初始化

---
+++ gmall-product/src/main/java/com/gmall/gmallproduct/GmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.gmall.gmallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallProductApplication.class, args);
+	}
+
+}
